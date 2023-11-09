2a0e33e8dbdb9921f19cac1d775f88ed0c4d628d
Empairim <titusfaulknertull@yahoo.com>
2023-11-09 11:11:41 -0500

    added cors

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import express from "express";
 import connectDB from "./database/db.js";
 import personRoutes from "./routes/personRoutes.js";
 import chalk from "chalk";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 connectDB();
 app.use(express.json());
+app.use(cors());
+
 app.use("/api", personRoutes);
 
 app.listen(PORT, () => {
